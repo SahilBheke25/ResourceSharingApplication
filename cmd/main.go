b92566c4704aa0ee1d753bc7849fa0b42244aaa3
sahilbheke <sahil.bheke@joshsoftware.com>
2025-02-02 22:16:33 +0530

    Resotre main and Completed user registration

--- cmd/main.go
+++ cmd/main.go
@@ -1,5 +1,28 @@
 package main
 
+import (
+	"fmt"
+	"log"
+	"net/http"
+
+	repository "github.com/SahilBheke25/ResourceSharingApplication/internal/Repository"
+	"github.com/SahilBheke25/ResourceSharingApplication/internal/app/login"
+	User "github.com/SahilBheke25/ResourceSharingApplication/internal/app/user"
+	_ "github.com/lib/pq"
+)
+
 func main() {
+
+	// Creating DB connection
+	repository.InitializeDatabase()
+	defer repository.DB.Close()
+
+	mux := http.DefaultServeMux
+	mux.HandleFunc("POST /login", login.Verify)
+	mux.HandleFunc("POST /register", login.Register)
+	mux.HandleFunc("POST /user", User.GetUserByIdHandler)
+
+	fmt.Println("listning to port 3000")
+	log.Fatal(http.ListenAndServe(":3000", mux))
 
 }

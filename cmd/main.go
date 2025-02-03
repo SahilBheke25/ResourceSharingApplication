d81a876b2b4fd3693937afa3a364351356acba1f
sahilbheke <sahil.bheke@joshsoftware.com>
2025-02-04 00:43:52 +0530

    Refactoring main

--- cmd/main.go
+++ cmd/main.go
@@ -5,15 +5,6 @@ import (
 	"log"
 	"net/http"
 
-	"fmt"
-	"log"
-	"net/http"
-
-	repository "github.com/SahilBheke25/ResourceSharingApplication/internal/Repository"
-	"github.com/SahilBheke25/ResourceSharingApplication/internal/app/login"
-	User "github.com/SahilBheke25/ResourceSharingApplication/internal/app/user"
-	_ "github.com/lib/pq"
-
 	Equipment "github.com/SahilBheke25/ResourceSharingApplication/internal/app/equipment"
 	"github.com/SahilBheke25/ResourceSharingApplication/internal/app/login"
 
@@ -23,18 +14,6 @@ import (
 )
 
 func main() {
-
-	// Creating DB connection
-	repository.InitializeDatabase()
-	defer repository.DB.Close()
-
-	mux := http.DefaultServeMux
-	mux.HandleFunc("POST /login", login.Verify)
-	mux.HandleFunc("POST /register", login.Register)
-	mux.HandleFunc("POST /user", User.GetUserByIdHandler)
-
-	fmt.Println("listning to port 3000")
-	log.Fatal(http.ListenAndServe(":3000", mux))
 
 	// Creating DB connection
 	repository.InitializeDatabase()

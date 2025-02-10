1d6c85a1e8fa3851d838f4beb7adb54022e6005a
sahilbheke <sahil.bheke@joshsoftware.com>
2025-02-10 23:19:59 +0530

    refactoring in main.go (added comments adn removed unnecessary code)

--- cmd/main.go
+++ cmd/main.go
@@ -18,18 +18,16 @@ func main() {
 	db := repository.InitializeDatabase()
 	defer db.Close()
 
+	// Initialize Dependencies
 	equipmentRepo := repository.NewEquipmentStore(db)
 	equipmentService := equipment.NewService(equipmentRepo)
 	equipmentHandler := equipment.NewHandler(equipmentService)
 
 	mux := http.DefaultServeMux
-	// mux.HandleFunc("POST /login", user.Verify)
-	// mux.HandleFunc("POST /register", user.Register)
+
+	// Routing
 	mux.HandleFunc("POST /equipments", equipmentHandler.CreateEquipmentHandler)
 	mux.HandleFunc("GET /equipments", equipmentHandler.ListEquipmentHandler)
-	// mux.HandleFunc("GET /equipments/{user_id}", equipment.GetEquipmentsByUserIdHandler)
-	// mux.HandleFunc("DELETE /equipments/{equipment_id}", equipment.DeleteEquipmentHandler)
-	// mux.HandleFunc("PUT /equipments/{equipment_id}", equipment.UpdateEquipmentHandler)
 
 	fmt.Println("listning to port 3000")
 	log.Fatal(http.ListenAndServe(":3000", mux))

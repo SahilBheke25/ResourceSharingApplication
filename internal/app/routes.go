10fe9ee8311acec981178d452231a2f7386ab181
sahilbheke <sahil.bheke@joshsoftware.com>
2025-03-02 12:46:18 +0530

    routes rearranged

--- internal/app/routes.go
+++ internal/app/routes.go
@@ -9,12 +9,12 @@ func InitializeRoutes(deps *Dependencies) http.Handler {
 	router.HandleFunc("POST /user/login", deps.userHandler.Login)
 	router.HandleFunc("POST /user/register", deps.userHandler.Register)
 	router.HandleFunc("POST /equipments", deps.equipmentHandler.CreateEquipmentHandler)
-	router.HandleFunc("GET /equipments", deps.equipmentHandler.ListEquipmentHandler)
-	router.HandleFunc("DELETE /equipments/{equipment_id}", deps.equipmentHandler.DeleteEquipmentHandler)
-	router.HandleFunc("PUT /equipments/{equipment_id}", deps.equipmentHandler.UpdateEquipmentHandler)
 	router.HandleFunc("POST /users/{user_id}/equipments/{equip_id}/rent", deps.rentalHandler.RentEquipment)
-	router.HandleFunc("GET /users/{user_id}/equipments/lended", deps.equipmentHandler.GetEquipmentsByUserIdHandler)
 	router.HandleFunc("GET /equipments/{equipment_id}", deps.equipmentHandler.EquipmentById)
+	router.HandleFunc("GET /equipments", deps.equipmentHandler.ListEquipmentHandler)
+	router.HandleFunc("GET /users/{user_id}/equipments/lended", deps.equipmentHandler.GetEquipmentsByUserIdHandler)
+	router.HandleFunc("PUT /equipments/{equipment_id}", deps.equipmentHandler.UpdateEquipmentHandler)
+	router.HandleFunc("DELETE /equipments/{equipment_id}", deps.equipmentHandler.DeleteEquipmentHandler)
 
 	return enableCORS(router)
 }
